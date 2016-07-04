bf25f104dd90e039669be1bda18502848b7a36ee
Jdc1197 <jdc1197@gmail.com>
2016-07-03 19:04:55 -0500

    removed outdated liscense

--- main.c
+++ main.c
@@ -1,19 +1,3 @@
-/* The authors of this work have released all rights to it and placed it
-in the public domain under the Creative Commons CC0 1.0 waiver
-(http://creativecommons.org/publicdomain/zero/1.0/).
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-ERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-Retrieved from: http://en.literateprograms.org/Hello_World_(C,_Cairo)?oldid=10388
-*/
-
-
 #include<cairo.h>
 #include<cairo-xlib.h>
 #include<X11/Xlib.h>
